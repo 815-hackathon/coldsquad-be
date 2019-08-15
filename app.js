a338aa9b4417e11c6647e129df39e5338409720f
Ryujeuk <jeuk817@gmail.com>
2019-08-15 15:57:05 +0900

    modified: error 처리 수정

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     res.status(err.status || 500);
-    res.render('error');
+    // res.render('error');
+    res.json({
+        message: err.message,
+        error: err
+    });
 });
 
 module.exports = app;
